6c4649387a2b90d2e6f5d45214dc64d66466b78a
natsuki-itou <n-itou7110@outlook.com>
2025-07-22 19:36:27 +0900

    initial commit

---
+++ eshacu-backend/src/main/java/com/eshacu/EshacuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eshacu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshacuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshacuBackendApplication.class, args);
+    }
+
+}
---
+++ eshacu-backend/src/test/java/com/eshacu/EshacuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshacu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshacuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
